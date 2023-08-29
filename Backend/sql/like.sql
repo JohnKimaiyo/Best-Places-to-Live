1b19788bf494346a2fc32e7fd2ba83720f6b9796
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-29 12:02:45 +0300

    final commt

---
+++ Backend/sql/like.sql
@@ -0,0 +1,16 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ProductID]
+      ,[Weight]
+      ,[FatContent]
+      ,[ProductVisibility]
+      ,[ProductType]
+      ,[MRP]
+      ,[Sales]
+      ,[Budgeted Sales]
+      ,[OutletID]
+      ,[Date]
+      ,[OutletSize]
+      ,[LocationType]
+      ,[OutletType]
+  FROM [Big Mart Sales].[dbo].['Big Mart Sales$']
+  WHERE [ProductType] like 'D%';
