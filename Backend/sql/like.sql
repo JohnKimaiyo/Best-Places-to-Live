de39cd0698d9970b25efb3458c8a05deab37d517
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-29 17:43:39 +0300

    like

--- Backend/sql/like.sql
+++ Backend/sql/like.sql
@@ -14,3 +14,14 @@ SELECT TOP (1000) [ProductID]
       ,[OutletType]
   FROM [Big Mart Sales].[dbo].['Big Mart Sales$']
   WHERE [ProductType] like 'D%';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [State]
+      ,[Overall rank]
+      ,[Affordability rank]
+      ,[Quality/cost of healthcare rank]
+      ,[Well-being rank]
+      ,[Weather rank]
+      ,[Crime rank]
+  FROM [Best places to live].[dbo].['Sheet 1$']
+  WHERE [State] like 'A%';
